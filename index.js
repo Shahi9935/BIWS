57dc6d0aa852bc3f2c451f22e59ac2364a0e1d9e
Aditya Narayan Shahi <adityan.shahi2014@gmail.com>
2024-12-23 17:28:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var basic = []
 var advanced = []
 var all = []
+var myarr = []
 var counter = 0;
+var totalCount = 0;
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i >= 0; i--) {
@@ -22,10 +24,13 @@ function csvToJson2(csv) {
       obj['question'] = question
       obj['answer'] = answer
       obj['topic'] = topic
+      obj['type'] = type
       if (type === 'BASIC'){
         basic.push(obj);
-      } else {
+      } else if (type === 'ADVANCED'){
         advanced.push(obj);
+      } else {
+        console.log(obj);
       }
       all.push(obj);
     });
@@ -35,14 +40,25 @@ function setQuestion(index) {
   const tEle = document.getElementById('topic');
   const qEle = document.getElementById('question');
   const aEle = document.getElementById('answer');
+  const dEle = document.getElementById('difficulty');
   const showEle = document.getElementById('show-answer');
   const hideEle = document.getElementById('hide-answer');
   aEle.style.display = 'none';
-  tEle.textContent = basic[index]['topic'];
-  qEle.textContent = basic[index]['question'].slice(3,-3);
-  aEle.textContent = basic[index]['answer'].slice(3,-1);
+  var arr = myarr;
+  tEle.textContent = arr[index]['topic'];
+  qEle.textContent = arr[index]['question'].slice(3,-3);
+  aEle.textContent = arr[index]['answer'].slice(3,-1);
+  dEle.textContent = arr[index]['type'];
+  if (arr[index]['type'] == 'BASIC'){
+    dEle.style.border="1px solid green";
+    dEle.style.color="green";
+  } else {
+    dEle.style.border="1px solid red";
+    dEle.style.color="red";
+  }
   showEle.style.display = 'block';
   hideEle.style.display = 'none';
+  document.getElementById('currentCount').textContent = index + 1;
 }
 
 function showAnswer(){
@@ -68,11 +84,54 @@ function nextQuestion(){
   setQuestion(counter);
 }
 
-  const jsonData = csvToJson2(csvData);
+function changeSubmit(){
+  var selectedTopic = document.getElementById('topicSelect').value;
+  var selectedDifficulty = document.getElementById('difficultySelect').value;
 
-  shuffleArray(advanced);
-  shuffleArray(basic);
-  shuffleArray(all);
+  if (selectedDifficulty === 'ALL' && selectedTopic === 'ALL'){
+    myarr = all;
+  } else {
+      if (selectedDifficulty === 'BASIC'){
+        if (selectedTopic === 'ALL'){
+          myarr = basic;
+        } else {
+          var temp = [];
+          all.forEach(ele => {
+            if (ele['type'] === 'BASIC' && ele['topic'] === selectedTopic){
+              temp.push(ele);
+            }
+          });
+          myarr = temp;
+        }
+      } else if (selectedDifficulty === 'ADVANCED'){
+        if (selectedTopic === 'ALL'){
+          myarr = advanced;
+        } else {
+          var temp = [];
+          all.forEach(ele => {
+            if (ele['type'] === 'ADVANCED' && ele['topic'] === selectedTopic){
+              temp.push(ele);
+            }
+          });
+          myarr = temp;
+        }
+      } else {
+        var temp = [];
+          all.forEach(ele => {
+            if (ele['topic'] === selectedTopic){
+              temp.push(ele);
+            }
+          });
+          myarr = temp;
+      }
+  }
 
+  totalCount = myarr.length;
+  document.getElementById('totalCount').textContent = totalCount;
+  shuffleArray(myarr);
+  counter = 0;
   setQuestion(0);
+}
 
+const jsonData = csvToJson2(csvData);
+changeSubmit();
